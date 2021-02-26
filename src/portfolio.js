b70c73710eaf63296e91c8c8cce8553bb78cdaa3
karimsalhi <karimsalhi.contact@gmail.com>
2021-02-26 16:09:54 +0100

    changed resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -15,7 +15,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1VMiSzOE13CccXB8i-p4PwbVkzJ6eLthi/view?usp=sharing",
+    "https://drive.google.com/file/d/1CPliWjN3UWpmxPQhVmi9gTQkqpfhfUeB/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
